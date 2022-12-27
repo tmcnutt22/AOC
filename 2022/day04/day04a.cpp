03bf3a9e3ee6ce9d97c6624e6784c42f080ca895
tom m <thomas.mcnutt.jr@gmail.com>
2022-12-27 13:41:18 -0500

    Day 4 completed

---
+++ 2022/day04/day04a.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main(void) {
+    std::string input = "input.txt";
+    std::fstream newFile(input);
+    std::string line;
+    std::vector<std::string> list;
+    //split by ,
+    while(getline(newFile, line)) {
+        list.push_back(line);
+    }
+    /*
+    //print the input vector
+    for (int i = 0; i < list.size(); i++) { 
+        std::cout << list.at(i) << std::endl;
+    }
+    */
+    //split by ','
+    std::vector <std::string> list2;
+    char delim = ',';
+    for (int i = 0; i < list.size(); i++) {
+        std::string temp = list.at(i);
+        std::size_t index = temp.find(delim);
+        list2.push_back(temp.substr(0, index));
+        list2.push_back(temp.substr(index + 1, temp.length()));
+    }
+    //split by -
+    std::vector <int> list_of_ints;
+    for (std::size_t i = 0; i < list2.size(); i++) {
+        std::string temp = list2.at(i);
+        std::size_t index = temp.find("-");
+        if (index != std::string::npos) {
+            std::string s1 = temp.substr(0, index);
+            std::string s2 = temp.substr(index + 1, temp.length());
+            /*
+            std::cout << "S1 val: " << s1 << std::endl;
+            std::cout << "S2 val: " << s2 << std::endl;
+            */
+            list_of_ints.push_back(std::stoi(s1));
+            list_of_ints.push_back(std::stoi(s2));
+        }
+
+    }
+    //check for the complete overlap
+    int over_lapping = 0;
+    int len = list_of_ints.size();
+    for (int i = 0; i < len; i += 4) {
+        int elf_1_start = list_of_ints.at(i); 
+        int elf_1_end = list_of_ints.at(i + 1); 
+        int elf_2_start = list_of_ints.at(i + 2); 
+        int elf_2_end = list_of_ints.at(i + 3); 
+        if (((elf_2_start >= elf_1_start) && (elf_2_end <= elf_1_end)) ||
+            ((elf_2_start <= elf_1_start) && (elf_2_end >= elf_1_end))
+            ) {
+            over_lapping++;
+        }
+    }
+    std::cout <<"Num of completely overlapping elves: " << over_lapping << std::endl;
+    
+    return 0;
+}
+
