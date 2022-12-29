0b6d6f224fca132bbe46ea8ade78ded374efdd18
tom <thomas.mcnutt.jr@gmail.com>
2022-12-29 13:09:39 -0500

    new pc

---
+++ .ccls-cache/@Users@tom@AoC/2022@day05@day05a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+
+    return 0;
+}
--- 2022/day05/day05a.cpp
+++ 2022/day05/day05a.cpp
@@ -3,6 +3,13 @@
 #include <string>
 
 int main() {
-
+    std::string line;
+    std::string input = "input.txt";
+    std::fstream newFile(input);
+    
+    while(getline(newFile, line)) {
+        std::cout << line << std::endl;
+    }
+    
     return 0;
 }
